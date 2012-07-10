18f4feaeda69f28d6c0f55beab2f8c07a68286b2
Christopher Biscardi <biscarch@sketcht.com>
2012-07-10 09:05:25 -0700

    .meta

--- platosite/routes/index.js
+++ platosite/routes/index.js
@@ -345,7 +345,8 @@ exports.createNewProject = function(req, res){
                       console.log('files exist');
                       console.log(files);
                       proj.folder = files[0];
-                      projects.update({_id: proj._id}, {$set:proj}, false, false);
+                      console.log(proj._id);
+                      projects.update({_id: proj._id}, {folder:files[0]}, false, false);
                       res.end('success!');
                     }
                   });

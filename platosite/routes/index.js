4739758056b5265d25897e1e5199875a9bee7fe7
Christopher Biscardi <biscarch@sketcht.com>
2012-07-10 12:38:38 -0700

    update

--- platosite/routes/index.js
+++ platosite/routes/index.js
@@ -373,7 +373,7 @@ exports.createNewProject = function(req, res){
 };
 
 exports.updateProject = function(req, res){
-  console.log('updating');
+  console.log('updating: ', req.params.pid);
   projects.find({_id:new ObjectId(req.params.pid)}, function(err, project){
     console.log('find');
     var rm = spawn('rm', ['rf', './projects/'+proj._id+'/*']);
